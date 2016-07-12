c3427fc43422c8c529ce11347dabbb3b4fb321af
Niels Nijens <nijens.niels@gmail.com>
2016-07-12 17:25:30 +0200

    Add SchemaElementInterface

---
+++ src/Schema/SchemaElementInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ConnectHolland\OpenAPISpecificationGenerator\Schema;
+
+/**
+ * SchemaElementInterface.
+ *
+ * @author Niels Nijens <niels@connectholland.nl>
+ */
+interface SchemaElementInterface
+{
+}
